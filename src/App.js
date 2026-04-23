110012d3412afe2727acb014ab8a8f1e28c97d28
eshaq18 <es7aq18@gmail.com>
2026-04-23 20:33:47 +0300

    restore api key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as XLSX from 'xlsx';
 import './App.css';
 
 const PASS = '059805';
-const API_KEY_STORED = process.env.REACT_APP_BREVO_KEY || '';
+const API_KEY_STORED = process.env.REACT_APP_BREVO_KEY || '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';
 const FROM_EMAIL = 'jobs@eshaqjob.store';
 
 const LOGO = () => (
